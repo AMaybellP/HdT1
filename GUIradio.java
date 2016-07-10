4730234b788c72e92756f54bec618399e6ec1305
AMaybellP <pen15127@uvg.edu.gt>
2016-07-09 23:41:05 -0600

    Update GUIradio.java

--- GUIradio.java
+++ GUIradio.java
@@ -41,179 +41,174 @@ public class GUIradio extends JFrame {
 	 */
 	public GUIradio() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 276);
+		setBounds(100, 100, 450, 301);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnOn = new JButton("ON");
+		JButton btnOn = new JButton("ON/OFF");
 		btnOn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				radio.encender();
+				radio.ON_OFF();
 			}
 		});
-		btnOn.setBounds(137, 11, 53, 23);
+		btnOn.setBounds(170, 11, 78, 23);
 		contentPane.add(btnOn);
-		
-		JButton btnOff = new JButton("OFF");
-		btnOff.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				radio.apagar();
-			}
-		});
-		btnOff.setBounds(218, 11, 53, 23);
-		contentPane.add(btnOff);
 		
 		JLabel lblJaja = new JLabel("");
 		lblJaja.setForeground(Color.WHITE);
 		lblJaja.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblJaja.setBackground(Color.WHITE);
-		lblJaja.setBounds(157, 71, 92, 23);
+		lblJaja.setBounds(157, 95, 92, 23);
 		contentPane.add(lblJaja);
 		
-		JRadioButton rdbtnAm = new JRadioButton("AM");
-		buttonGroup.add(rdbtnAm);
-		rdbtnAm.setBounds(147, 41, 41, 23);
-		contentPane.add(rdbtnAm);
-		
-		JRadioButton rdbtnFm = new JRadioButton("FM");
-		buttonGroup.add(rdbtnFm);
-		rdbtnFm.setBounds(218, 41, 53, 23);
-		contentPane.add(rdbtnFm);
-		
 		JButton button = new JButton("<<<");
-		button.setBounds(78, 71, 57, 23);
+		button.setBounds(78, 95, 57, 23);
 		contentPane.add(button);
 		
 		JButton button_1 = new JButton(">>>");
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				if(rdbtnAm.isSelected())
-				{
-					radio.cambiar("AM");
-				}
-				if(rdbtnFm.isSelected())
-				{
-					radio.cambiar("FM");
-				}
-				radio.avanzar();
-				lblJaja.setText(radio.getEstacion());
+				radio.Cambio_de_Estacion();
+				// lblJaja.setText(radio.getEstacion());
 			}
 		});
-		button_1.setBounds(270, 71, 57, 23);
+		button_1.setBounds(270, 95, 57, 23);
 		contentPane.add(button_1);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(189, 105, 28, 20);
+		comboBox.setBounds(189, 129, 28, 20);
 		contentPane.add(comboBox);
 		
 		JButton button_2 = new JButton("1");
 		button_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				radio.guardar(0, Float.parseFloat(lblJaja.getText()));
+				radio.Guardar_Estacion();
+				radio.Seleccionar_estacion_guardada();
 			}
 		});
-		button_2.setBounds(37, 150, 46, 23);
+		button_2.setBounds(37, 174, 46, 23);
 		contentPane.add(button_2);
 		
 		JButton button_3 = new JButton("2");
 		button_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				radio.guardar(1, Float.parseFloat(lblJaja.getText()));
+				radio.Guardar_Estacion();
+				radio.Seleccionar_estacion_guardada();
 			}
 		});
-		button_3.setBounds(95, 150, 46, 23);
+		button_3.setBounds(95, 174, 46, 23);
 		contentPane.add(button_3);
 		
 		JButton button_4 = new JButton("3");
 		button_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				radio.guardar(2, Float.parseFloat(lblJaja.getText()));
+				radio.Guardar_Estacion();
+				radio.Seleccionar_estacion_guardada();
 			}
 		});
-		button_4.setBounds(154, 150, 46, 23);
+		button_4.setBounds(154, 174, 46, 23);
 		contentPane.add(button_4);
 		
 		JButton button_5 = new JButton("4");
 		button_5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				radio.guardar(3, Float.parseFloat(lblJaja.getText()));
+				radio.Guardar_Estacion();
+				radio.Seleccionar_estacion_guardada();
 			}
 		});
-		button_5.setBounds(216, 150, 46, 23);
+		button_5.setBounds(216, 174, 46, 23);
 		contentPane.add(button_5);
 		
 		JButton button_6 = new JButton("5");
 		button_6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				radio.guardar(4, Float.parseFloat(lblJaja.getText()));
+				radio.Guardar_Estacion();
+				radio.Seleccionar_estacion_guardada();
 			}
 		});
-		button_6.setBounds(274, 150, 46, 23);
+		button_6.setBounds(274, 174, 46, 23);
 		contentPane.add(button_6);
 		
 		JButton button_7 = new JButton("6");
 		button_7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				radio.guardar(5, Float.parseFloat(lblJaja.getText()));
+				radio.Guardar_Estacion();
+				radio.Seleccionar_estacion_guardada();
 			}
 		});
-		button_7.setBounds(333, 150, 46, 23);
+		button_7.setBounds(333, 174, 46, 23);
 		contentPane.add(button_7);
 		
 		JButton button_8 = new JButton("7");
 		button_8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				radio.guardar(6, Float.parseFloat(lblJaja.getText()));
+				radio.Guardar_Estacion();
+				radio.Seleccionar_estacion_guardada();
 			}
 		});
-		button_8.setBounds(37, 186, 46, 23);
+		button_8.setBounds(37, 210, 46, 23);
 		contentPane.add(button_8);
 		
 		JButton button_9 = new JButton("8");
 		button_9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				radio.guardar(7, Float.parseFloat(lblJaja.getText()));
+				radio.Guardar_Estacion();
+				radio.Seleccionar_estacion_guardada();
 			}
 		});
-		button_9.setBounds(95, 186, 46, 23);
+		button_9.setBounds(95, 210, 46, 23);
 		contentPane.add(button_9);
 		
 		JButton button_10 = new JButton("9");
 		button_10.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				radio.guardar(8, Float.parseFloat(lblJaja.getText()));
+				radio.Guardar_Estacion();
+				radio.Seleccionar_estacion_guardada();
 			}
 		});
-		button_10.setBounds(154, 186, 46, 23);
+		button_10.setBounds(154, 210, 46, 23);
 		contentPane.add(button_10);
 		
 		JButton button_11 = new JButton("10");
 		button_11.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				radio.guardar(9, Float.parseFloat(lblJaja.getText()));
+				radio.Guardar_Estacion();
+				radio.Seleccionar_estacion_guardada();
 			}
 		});
-		button_11.setBounds(216, 186, 46, 23);
+		button_11.setBounds(216, 210, 46, 23);
 		contentPane.add(button_11);
 		
 		JButton button_12 = new JButton("11");
 		button_12.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				radio.guardar(10, Float.parseFloat(lblJaja.getText()));
+				radio.Guardar_Estacion();
+				radio.Seleccionar_estacion_guardada();
 			}
 		});
-		button_12.setBounds(274, 186, 46, 23);
+		button_12.setBounds(274, 210, 46, 23);
 		contentPane.add(button_12);
 		
 		JButton button_13 = new JButton("12");
 		button_13.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				radio.guardar(11, Float.parseFloat(lblJaja.getText()));
+				radio.Guardar_Estacion();
+				radio.Seleccionar_estacion_guardada();
 			}
 		});
-		button_13.setBounds(333, 186, 46, 23);
+		button_13.setBounds(332, 210, 46, 23);
 		contentPane.add(button_13);
+		
+		JButton button_14 = new JButton("AM/FM");
+		button_14.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				radio.AM_FM();
+			}
+		});
+		button_14.setBounds(170, 45, 78, 23);
+		contentPane.add(button_14);
+		
 	}
 }

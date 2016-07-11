5ccda6fa6f6b60f3d04ab1294e9cf9854c809a97
Jose Molina <mol14492@uvg.edu.gt>
2016-07-11 09:48:52 -0600

    Update Radio.java

--- Radio.java
+++ Radio.java
@@ -4,6 +4,7 @@ public interface Radio
 	public void ON_OFF();
 	public void AM_FM();
 	public void Cambio_de_Estacion();
+	public void Cambiar_Emisora();
 	public void Guardar_Estacion();
 	public void Seleccionar_estacion_guardada();
 	public boolean Guardar_seleccion();

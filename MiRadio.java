c3d876e1e4c3cc75da59b8aa6c8aba62acd53a95
AMaybellP <pen15127@uvg.edu.gt>
2016-07-11 20:45:49 -0600

    Update MiRadio.java

--- MiRadio.java
+++ MiRadio.java
@@ -4,9 +4,9 @@ public class MiRadio implements Radio
 	/***atributos***/
 	private boolean encendido = false;
 	private boolean guardar = true;
-	private float [] botones= new float[12];
+	private double [] botones= new double[12];
 	private boolean frecuencia = true;
-	private float estacion=530f;
+	private double estacion=530;
 	private int boton;
 	
 	/***métodos***/
@@ -22,10 +22,10 @@ public class MiRadio implements Radio
 	public void setEncendido(boolean encendido) {
 		this.encendido = encendido;
 	}
-	public float[] getBotones() {
+	public double[] getBotones() {
 		return botones;
 	}
-	public void setBotones(float[] botones) {
+	public void setBotones(double[] botones) {
 		this.botones = botones;
 	}
 	public boolean getFrecuencia() {
@@ -34,10 +34,10 @@ public class MiRadio implements Radio
 	public void setFrecuencia(boolean frecuencia) {
 		this.frecuencia = frecuencia;
 	}
-	public float getEstacion() {
+	public double getEstacion() {
 		return estacion;
 	}
-	public void setEstacion(float estacion) {
+	public void setEstacion(double estacion) {
 		this.estacion = estacion;
 	}
 	public int getBoton() {
@@ -77,69 +77,106 @@ public class MiRadio implements Radio
 		
 	}
 	@Override
-	public float Cambio_de_Estacion() {
+	public void Cambio_de_Emisora(boolean a) {
 		if (encendido==true){
 		// TODO Auto-generated method stub
 		{
 			boolean F= getFrecuencia();
 		}
 		
-		if (frecuencia==true)
+		if (a==true)
 		{
-			if (estacion%10!=0)
+			if (frecuencia==true)
 			{
-				estacion=530f;
-			}
-		
-			if(estacion<1610)
-			{
-				estacion= estacion+10f;
-			}
-			else
-			{
-				estacion=530f;
-			}
-		}
-		if(frecuencia==false)
-		{
-			if(estacion%10==0)
-			{
-				estacion= 87.90f;
-			}
+				if (estacion%10!=0)
+				{
+					estacion=530;
+				}
 			
-			if(estacion<107.9)
-			{
-				estacion= estacion+0.20f;
+				if(estacion<1610)
+				{
+					estacion= estacion+10;
+				}
+				else
+				{
+					estacion=530;
+				}
 			}
-			else
+			if(frecuencia==false)
 			{
-				estacion=87.90f;
+				if(estacion%10==0)
+				{
+					estacion= 87.9;
+				}
+				
+				if(estacion<107.9)
+				{
+					estacion= estacion+0.2;
+				}
+				else
+				{
+					estacion=87.9;
+				}
 			}
 		}
+		else {
+			if (frecuencia==true)
+			{
+				if (estacion%10!=0)
+				{
+					estacion=1610;
+				}
+			
+				if(estacion>530)
+				{
+					estacion= estacion-10;
+				}
+				else
+				{
+					estacion=1610;
+				}
+			}
+			if(frecuencia==false)
+			{
+				if(estacion%10==0)
+				{
+					estacion= 107.9;
+				}
+				
+				if(estacion>87.9)
+				{
+					estacion= estacion-0.2;
+				}
+				else
+				{
+					estacion=107.9;
+				}
+			}}
+			
 		setEstacion(estacion);
 		}
-		return (Math.round(estacion*1000f))/1000f;
+		//return (Math.round(estacion*1000f))/1000f;
 	}
 	@Override
-	public void Guardar_Estacion(int bot) {
+	public void Guardar_Emisora(int bot) {
 		if (encendido==true){
 		// TODO Auto-generated method stub
 		if (guardar == true)
 		{
-			float est= getEstacion();
+			double est= getEstacion();
 			botones[bot]= est;
 		}}
 	}
 	@Override
-	public float Seleccionar_estacion_guardada(int bot) {
+	public void Seleccionar_emisora_guardada(int bot) {
 		if (encendido==true){
 		// TODO Auto-generated method stub
 		if (guardar==false)
 		{
-			float est= botones[bot];
+			double est= botones[bot];
 			setEstacion(est);
 		}}
-		return (Math.round(estacion*1000f))/1000f;
+		//return (Math.round(estacion*1000f))/1000f;
 	}
 	@Override
 	public void Guardar_seleccion() {
@@ -153,5 +190,14 @@ public class MiRadio implements Radio
 		{
 			guardar= true;
 		}}
+	}
+	@Override
+	public double getEmisora() {
+		// TODO Auto-generated method stub
+		/*double est= estacion*1000;
+		Math.round(est);
+		return est/1000;*/
+		//return (Math.round(estacion*1000))/1000;
+		return estacion;
 	}
 }

e4c91cb2c9d6cd298478f3445b3cc3271071310c
AMaybellP <pen15127@uvg.edu.gt>
2016-07-11 20:47:16 -0600

    Update Radio.java

--- Radio.java
+++ Radio.java
@@ -1,9 +1,12 @@
+
 public interface Radio 
 {	
 	public void ON_OFF();
 	public void AM_FM();
-	public float Cambio_de_Estacion();
-	public void Guardar_Estacion(int bot);
-	public float Seleccionar_estacion_guardada(int bot);
+	public void Cambio_de_Emisora(boolean a);
+	public void Guardar_Emisora(int x);
+	public void Seleccionar_emisora_guardada(int x);
 	public void Guardar_seleccion();
+	public double getEmisora();
 }
+
